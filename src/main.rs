4141206f25b4bec3fb7717f20d9d2b66eb873c07
Strawby <staubfingercodinggames@gmail.com>
2024-02-04 12:03:28 +0100

    Add Basic Argument Parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 use crate::chess::position::Position;
 use crate::chess::r#move::Move;
+use clap::{Parser, Subcommand};
 
 mod chess;
+mod cli;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct CLI {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Play against the engine on the Command Line
+    CLI,
+    /// Play against another Person on the Command Lina
+    TwoPlayer,
+    /// Run the engine in uci mode, this is the default behavior when no subcommand is supplied
+    UCI,
+}
 
 fn main() {
+    let cli = CLI::parse();
+    println!("{:?}", cli);
+
     let start = Move::nop_move();
     let first_move = Move::new(
         start.clone(),
